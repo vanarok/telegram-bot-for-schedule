c8614abba03b5338b1b910bcccb11dcbab10c620
vanarok <ivanisekeev0@gmail.com>
2021-09-21 22:55:48 +0400

    Add convert pdf to image

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
-require('dotenv').config();
-const https = require('https');
-const TelegramBot = require('node-telegram-bot-api');
+import 'dotenv/config';
+import got from 'got';
+import TelegramBot from 'node-telegram-bot-api';
+
 const telegramToken = process.env.TELEGRAM_TOKEN;
 const bot = new TelegramBot(telegramToken, {polling: true});
-const chatId = -1001541014190; //831526627
-const url = 'https://ciur.ru/stmit/commondocs/';
-const {ImageMagick} = require('pdf-images');
+const chatId = 831526627; //-1001541014190;
 const dateSchedule = new Date();
+
+dateSchedule.setDate(dateSchedule.getDate() + 1);
+
+setInterval(() => {
+  let nameFile = `Расписание ${dateSchedule.getDate()}.${dateSchedule.toLocaleString(
+      'default',
+      {month: '2-digit'})}.${dateSchedule.getFullYear()}.pdf`;
+  nameFile = encodeURIComponent(nameFile);
+
+  let req = https.request(url + nameFile,
+      (res) => {
+        if (res.statusCode === 200) {
+          let images = ImageMagick.convert(url +
+              nameFile, '/tmp', 'img').images;
+
+          console.log(images);
+          for (let i in images) {
+            bot.sendPhoto(chatId, images[i]);
+          }
+          dateSchedule.setDate(dateSchedule.getDate() + 1);
+        }
+      },
+  );
+  req.end();
+}, 600000);
 
 bot.onText(/\/id/, async (msg) => {
   await bot.sendMessage(msg.chat.id, msg.chat.id);
 });
 
-bot.onText(/расписание (.+)/, async (msg, match) => {
+bot.onText(/\/расписание (.+)/, async (msg, match) => {
   let images = await schedule(match[1]);
   if (typeof images === 'string') {
     await bot.sendMessage(msg.chat.id, images);
@@ -62,27 +86,45 @@ function schedule(match) {
   });
 }
 
-dateSchedule.setDate(dateSchedule.getDate()+1);
+function getSchedule(text) {
+  let siteOne = req(
+      `https://ciur.ru/stmit/commondocs/${nameFileSchedule(text)}`);
+  let siteTwo = req(
+      `http://stmit.ru/wp-content/uploads/2021/09/${nameFileSchedule(text)}`);
+  if (siteOne) {
 
-setInterval(() => {
-  let nameFile = `Расписание ${dateSchedule.getDate()}.${dateSchedule.toLocaleString(
-      'default',
-      {month: '2-digit'})}.${dateSchedule.getFullYear()}.pdf`;
-  nameFile = encodeURIComponent(nameFile);
+  }
+}
 
-  let req = https.request(url + nameFile,
-      (res) => {
-        if (res.statusCode === 200) {
-          let images = ImageMagick.convert(url +
-              nameFile, '/tmp', 'img').images;
+function nameFileSchedule(text) {
+  let f = undefined;
+  if (text === 'сегодня') {
+    f = `Расписание ${date.getDate()}.${date.toLocaleString('default',
+        {month: '2-digit'})}.${date.getFullYear()}.pdf`;
+  }
+  if (text === 'завтра') {
+    f = `Расписание ${date.getDate() + 1}.${date.toLocaleString(
+        'default',
+        {month: '2-digit'})}.${date.getFullYear()}.pdf`;
+  }
+  if (text === 'послезавтра') {
+    f = `Расписание ${date.getDate() + 2}.${date.toLocaleString(
+        'default',
+        {month: '2-digit'})}.${date.getFullYear()}.pdf`;
+  }
+  return f;
+}
 
-          console.log(images);
-          for (let i in images) {
-            bot.sendPhoto(chatId, images[i]);
-          }
-          dateSchedule.setDate(dateSchedule.getDate() + 1);
-        }
-      },
-  );
-  req.end();
-}, 600000);
+/**
+ * @param {object} option
+ */
+
+function req(option) {
+  try {
+    return got(option);
+  } catch (e) {
+    console.log(e);
+    return false;
+  }
+
+}
